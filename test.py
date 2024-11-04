35c679d4cccf65aa0a6065f5c2802ae80cbee533
av2884 <abhiveerhome@gmail.com>
2024-11-04 05:36:20 +0000

    normalized x_train

--- test.py
+++ test.py
@@ -4,13 +4,11 @@ np1 = np.array([1,2,3,4,5,6,7,8,9])
 print(np1)
 print(np1.shape)
 
-# np2 = np1.reshape(3,3,3)
+# np2 = np1.reshape()
 # print(np2)
 # print(np2.shape)
 
-
-np4 = np.array([[[1,2,3,4] , [1,2,3], [1,2,3] , [1,2,3]],
-                [[1,2,3] , [1,2,3], [1,2,3] , [1,2,3]],
-                [[1,2,3] , [1,2,3], [1,2,3] , [1,2,3]]])
-print(np4)
-print(np4.shape)
+np3 = np.zeros((2,2,2,2))
+ 
+y = np.where(np3 % 1 == 1)
+print(f"y = {y}")
