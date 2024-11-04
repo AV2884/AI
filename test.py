2f94ba161a8b6e8efeeb50496240c79ca11e5fce
av2884 <abhiveerhome@gmail.com>
2024-11-04 03:53:25 +0000

    started NN, refining data

---
+++ test.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+np1 = np.array([1,2,3,4,5,6,7,8,9])
+print(np1)
+print(np1.shape)
+
+# np2 = np1.reshape(3,3,3)
+# print(np2)
+# print(np2.shape)
+
+
+np4 = np.array([[[1,2,3,4] , [1,2,3], [1,2,3] , [1,2,3]],
+                [[1,2,3] , [1,2,3], [1,2,3] , [1,2,3]],
+                [[1,2,3] , [1,2,3], [1,2,3] , [1,2,3]]])
+print(np4)
+print(np4.shape)
