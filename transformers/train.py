4bbeb16ba873aebc3421b7ea07f442886f9da220
av2884 <abhiveerhome@gmail.com>
2024-12-28 10:16:07 +0000

    data handling

---
+++ transformers/train.py
@@ -0,0 +1,9 @@
+import subprocess
+
+# Run the wget command
+subprocess.run(["wget", "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"])
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+print("length of dataset in characters: ", len(text))
