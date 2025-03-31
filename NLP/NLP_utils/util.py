22cb39200b7402a089942c13c94c78f6e10dbfc5
av2884 <abhiveerhome@gmail.com>
2025-03-31 08:13:21 +0000

    Cleaned folder. Naive Bayes init

--- NLP/NLP_utils/util.py
+++ NLP/NLP_utils/util.py
@@ -35,3 +35,36 @@ def build_freqs(tweets, ys):
             pair = (word, y)  
             freqs[pair] = freqs.get(pair, 0) + 1 
     return freqs
+
+
+
+
+
+
+
+
+
+
+
+
+
+def get_ratio(freqs, word):
+    '''
+    Input:
+        freqs: dictionary containing the words
+
+    Output: a dictionary with keys 'positive', 'negative', and 'ratio'.
+        Example: {'positive': 10, 'negative': 20, 'ratio': 0.5}
+    '''
+    pos_neg_ratio = {'positive': 0, 'negative': 0, 'ratio': 0.0}
+    ### START CODE HERE ###
+    # use lookup() to find positive counts for the word (denoted by the integer 1)
+    pos_neg_ratio['positive'] = (lookup(freqs,word,1) )
+    
+    # use lookup() to find negative counts for the word (denoted by integer 0)
+    pos_neg_ratio['negative'] = lookup(freqs,word,0)
+    
+    # calculate the ratio of positive to negative counts for the word
+    pos_neg_ratio['ratio'] = pos_neg_ratio['positive'] / pos_neg_ratio['negative']
+    ### END CODE HERE ###
+    return pos_neg_ratio
